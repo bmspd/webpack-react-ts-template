5c552f597b094c7b64edaa00e0704d7fea1d2b4a
Vladimir <my_spid@mail.ru>
2022-05-13 01:15:56 +0300

    add picture, pure css files and environments handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
 export const App = () => {
-    return <h1>Hello :')</h1>
+    return <>
+        <h1>Hello :')</h1>
+        <h1>{process.env.NODE_ENV} - {process.env.name}</h1>
+        <img src={IMAGE} alt="React logo" width="200"/>
+        <img src={LOGO} alt="React logo" width="100"/>
+    </>
 }
