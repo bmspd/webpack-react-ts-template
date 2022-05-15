d50998ca5f87a5bfe62be45ce6767f9c1635e4f0
Vladimir <my_spid@mail.ru>
2022-05-15 21:56:34 +0300

    add react refresh plugin, for saving states in hot reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import IMAGE from './react.png'
 import LOGO from './logo.svg'
+import ClickCounter from "./ClickCounter";
 export const App = () => {
     return <>
         <h1>Hello :')</h1>
         <h1>{process.env.NODE_ENV} - {process.env.name}</h1>
         <img src={IMAGE} alt="React logo" width="200"/>
         <img src={LOGO} alt="React logo" width="100"/>
+        <ClickCounter />
     </>
 }
